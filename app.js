efdf1d8c3038c287f848c6ad3f6fddb7343e3bb5
diegoami <diego.amicabile@gmail.com>
2018-07-08 16:39:46 +0200

    Fixed withdrawal of stuff

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 var _ = require('underscore')
+//00:00:55,146
+//00:01:29,803
 
+var convert_to_time = function(str) {
+    let tstr = str.trim()
+    let hours = parseInt(tstr.substr(0,2))
+    let minutes = parseInt(tstr.substr(3,2))
+    let seconds = parseInt(tstr.substr(6,2))
+    let millis = parseFloat(tstr.substr(9,3))
+    return (seconds + minutes * 60 + hours * 3600) + millis / 1000
+}
 
 var convert_subtitles = function(subtitles_srt) {
-    let subtitles_lines = subtitles_srt.split('\r\n')
-    let subtitles_objs = []
+    let subtitles_lines = subtitles_srt.split('\r\n');
+    let subtitles_objs = [];
     var line_status = 0;
     for (i = 0; i < subtitles_lines.length; i += 4) {
         if (i + 2 < subtitles_lines.length) {
             subtitle_obj = {};
             let time_span = subtitles_lines[i+1];
             let timespans = time_span.split('-->')
-            subtitle_obj["px_start"] = timespans[0].trim().substr(6).replace(",","") / 1000;
-            subtitle_obj["px_end"] = timespans[1].trim().substr(6).replace(",","") / 1000;
+            subtitle_obj["px_start"] = convert_to_time(timespans[0]);
+            if (timespans.length > 0 && timespans[1] ) {
+                subtitle_obj["px_end"] = convert_to_time(timespans[1]);
+            } else {
+                subtitle_obj["px_end"] = 1000
+            }
 
             subtitle_obj["text"] = subtitles_lines[i+2];
             subtitles_objs.push(subtitle_obj);
