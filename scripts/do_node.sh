ce1c2dce7ce71f6ef8154a877ed633403f6c3123
Diego Amicabile <diego.amicabile@gmail.com>
2019-11-13 17:33:14 +0000

    updated json for songs

--- scripts/do_node.sh
+++ scripts/do_node.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/projects/JS_musicblogs/
 node update_files.js
 scp json/* ubuntu@18.185.30.140:/home/ubuntu/projects/JS_FE_musicblogs/public
-git commit -a
+git commit -a -m "updated json for songs"
 git push origin master
 
